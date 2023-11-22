c3cbdcb388ee0f43c76d1831c3bc507e5cd6d7d8
Robin Kelsick II <robinkelsick@gmail.com>
2023-11-21 21:48:15 -0500

    set resolver for tracksForHome Query

---
+++ server/src/resolvers.js
@@ -0,0 +1,8 @@
+const resolvers = {
+  Query: {
+    // returns an array of tracks that will be used to populate the homepage grid of our web client
+    tracksForHome: () => {},
+  },
+};
+
+module.exports = resolvers;
