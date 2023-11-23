ffd225c3606b4e2f8583a8269a1830d36052016d
Robin Kelsick II <robinkelsick@gmail.com>
2023-11-22 23:08:39 -0500

    finished the resolver that returns tracks.

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,7 +1,9 @@
 const resolvers = {
   Query: {
     // returns an array of tracks that will be used to populate the homepage grid of our web client
-    tracksForHome: () => {},
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTrackForHome();
+    },
   },
 };
 
