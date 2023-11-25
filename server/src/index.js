bb55f4ac63612a2107715be3ab8e83d52a5747f3
Robin Kelsick II <robinkelsick@gmail.com>
2023-11-24 23:39:19 -0500

    fixed the issue with trackAPI returning null

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,30 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
-const resolvers = require('./resolvers');
-const TrackAPI = require('./datasources/track-api');
+
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+const TrackAPI = require('./datasources/track-api');
 
 async function startApolloServer() {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => {
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+
       return {
-        trackAPI: new TrackAPI(),
+        dataSources: {
+          trackAPI: new TrackAPI({ cache }),
+        },
       };
     },
   });
-  const { url } = await startStandaloneServer(server);
 
   console.log(`
-      🚀  Server is running
-      📭  Query at ${url}
-    `);
+    🚀  Server is running
+    📭  Query at ${url}
+  `);
 }
 
 startApolloServer();
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,6 +1,6 @@
 const resolvers = {
   Query: {
-    // returns an array of tracks that will be used to populate the homepage grid of our web client
+    // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
